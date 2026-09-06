190053e4da0d5b0e5c75714d3f92ac72e1ff98a2
yuu <onsokuaniki6@gmail.com>
2026-09-06 23:58:44 +0900

    SimpleCovを導入してカバレッジ計測を追加

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,13 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require "simplecov"
+
+SimpleCov.start "rails" do
+  # テストコード自体は計測対象から外す
+  add_filter "/spec/"
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
