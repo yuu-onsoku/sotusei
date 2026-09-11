c90e1ebb9a1bedaa56d6f47d50e1581ed71945d4
yuu <onsokuaniki6@gmail.com>
2026-09-11 23:33:27 +0900

    他人の質問を削除できないことのリクエストspecを追加

--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -76,4 +76,21 @@ RSpec.describe "Questions", type: :request do
       end
     end
   end
+
+  describe "DELETE /questions/:id" do
+    context "他人の質問を消去しようとしたとき" do
+      it "消去されず、一覧にリダイレクトされる" do
+        owner = create(:user)
+        question = create(:question, user: owner)
+
+        other = create(:user)
+        sign_in other
+
+        delete question_path(question)
+
+        expect(Question.count).to eq(1)
+        expect(response).to redirect_to(questions_path)
+      end
+    end
+  end
 end
